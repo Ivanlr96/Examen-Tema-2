992f456abc36d0de65efd19a079b545ae741fcd7
unknown <ivanlorenzoruiz@alumnos.tuniversformacion.es>
2023-10-30 09:15:07 +0100

    Creada la función retirar a partir de la de depositar e implementada al botón correspondiente

--- scripts/index.js
+++ scripts/index.js
@@ -20,11 +20,22 @@ const depositar= ()=> {
         document.getElementById("saldo").textContent=saldoActual+"€";
     }
 }
+const retirar=()=>{
+    const cantidad=parseFloat(prompt("Inserte la cantidad a retirar"))
+    if (isNaN(cantidad) || cantidad<=0 || cantidad>saldoInicial) {
+        alert ("Inserte una cantidad valida")
+    }
+    else {
+        saldoActual=saldoInicial-cantidad;
+        saldoInicial=saldoActual
+        alert(`Se han retirado ${cantidad}€`)
+        document.getElementById("saldo").textContent=saldoActual+"€";
+    }
+}
 botonDepositar.addEventListener("click", depositar)
 
-botonRetirar.addEventListener("click", ()=>{
+botonRetirar.addEventListener("click", retirar)
 
-})
 
 botonTransferir.addEventListener("click", ()=>{
 
