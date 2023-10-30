8f025bab6c443cacbbc30e94912031229e2f0a64
unknown <ivanlorenzoruiz@alumnos.tuniversformacion.es>
2023-10-30 09:03:29 +0100

    Declaradas las variables principales y realizadas las capturas de los ids para las botones en el archivo JS

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+
+const botonDepositar=document.getElementById("btnD");
+const botonRetirar=document.getElementById("btnR");
+const botonTransferir=document.getElementById("btnT");
+const botonCambiar=document.getElementById("btnC");
+const botonSalir=document.getElementById("btnS");
+let saldoInicial=1000, saldoActual, intentos=3;
+const PIN_CORRECTO="1234";
+
