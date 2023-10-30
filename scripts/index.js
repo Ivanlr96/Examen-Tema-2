33049c70147f5a7d98a7c1738adce69c3ad5912c
unknown <ivanlorenzoruiz@alumnos.tuniversformacion.es>
2023-10-30 09:25:06 +0100

    Creada la función Iniciar Sesión, creada la página de bloqueo para que se redireccione si los intentos llegan a 0 e implementada para que se ejejecute cuando se cargue la página

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,21 @@ let saldoInicial=1000, saldoActual, intentos=3;
 const PIN_CORRECTO="1234";
 document.getElementById("saldo").textContent=saldoInicial+"€";
 
+const iniciarSesion=()=> {
+    const pin=prompt("Inserte el pin")
+    while (pin!=PIN_CORRECTO && intentos>1) {
+        intentos--
+        alert(`Pin incorrecto, quedan ${intentos}`)
+        const pin=prompt("Inserte el pin")
+    }
+    if (pin===PIN_CORRECTO) {
+        alert("Inicio de sesión exitoso, bienvenido")
+    }
+    else {
+        window.location.replace("templates/bloqueado.html")
+    }
+}
+
 const depositar= ()=> {
     const cantidad=parseFloat(prompt("Inserte la cantidad a depositar"))
     if (isNaN(cantidad) || cantidad<=0) {
@@ -48,3 +63,5 @@ botonCambiar.addEventListener("click", ()=>{
 botonSalir.addEventListener("click", ()=>{
 
 })
+
+window.addEventListener("load",iniciarSesion)
